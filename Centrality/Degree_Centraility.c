087dbe42a4a998b47716d126e3cb8929aeb83a65
a7medfawzy97 <36432072+a7medfawzy97@users.noreply.github.com>
2019-06-27 19:30:01 +0200

    Update Degree_Centraility.c

--- Centrality/Degree_Centraility.c
+++ Centrality/Degree_Centraility.c
@@ -12,10 +12,12 @@ int main()
 	// Un d i r e c t e d Weighted
 	//freopen("datafile.txt", "r", stdin);
 	int n, m;
+	//entering the nodes and edges numbers
 	cin >> n >> m;
-	vector<vector<int> > adjmatrix(n, vector<int>(n, 0));
+	//vector<vector<int> > adjmatrix(n, vector<int>(n, 0));
 	vector<list<pair<int, int>>> adj_list(n);
 	int a, b, c;
+	//entering source ,destination and weight
 	for (int i = 0; i < m; i++)
 	{
 		cin >> a >> b >> c;
@@ -26,16 +28,18 @@ int main()
 		p2.second = c;
 		adj_list[a].push_back(p1);
 		adj_list[b].push_back(p2);
-		adjmatrix[a][b] = c;
-		adjmatrix[b][a] = c;
+		//adjmatrix[a][b] = c;
+		//adjmatrix[b][a] = c;
 	}
 
-
+	//printing degree centraility
 	for (int j = 0; j < n; j++)
 	{
-		cout << j << "has edges degree equals" << adj_list[j].size() << endl;
+		cout << adj_list[j].size() << "\n";
 	}
 
 	system("pause");
 	return 0;
 }
+
+
