04a8ee4389be6b287ceaabc655f2bbce16ea5afd
Baher Abdelfatah <36481333+BaherAbdelfatah@users.noreply.github.com>
2019-06-29 00:10:58 +0200

    Visualization Using Python NetworkX

---
+++ Visualization/visualization_degree.py
@@ -0,0 +1,10 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G=nx.Graph()
+nodes = nx.read_adjlist("nodes.txt")
+G.add_nodes_from(nodes.nodes())
+G = nx.read_edgelist("edges.txt", create_using =nx.MultiGraph(), nodetype = int,data=[('weight',int)])
+d = dict(nx.degree_centrality(G))
+nx.draw(G, nodelist=d.keys(), node_size=[v * 500 for v in d.values()])
+plt.show()
